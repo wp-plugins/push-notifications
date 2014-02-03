0bbdc8323a100080e1578c73213ee201f334dd14
pushwoosh <pushwoosh@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-03 09:01:28 +0000

    tagging version 2.2
    git-svn-id: https://plugins.svn.wordpress.org/push-notifications/tags/2.2@850399 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- pushwoosh.php
+++ pushwoosh.php
@@ -1,28 +1,34 @@
 <?php
 
- 	/**
- 	 * Plugin Name: Pushwoosh
- 	 * Plugin URI: http://pushwoosh.com
- 	 * Description: Push notifications plugin for wordpress by Pushwoosh
- 	 * Author: Arello Mobile
- 	 * Author URI: http://www.arello-mobile.com/
-	 *
-	 * Copyright 2013  Arello Mobile  (email: support@arello-mobile.com)
-	 * This program is free software; you can redistribute it and/or modify
-	 * it under the terms of the GNU General Public License as published by
-	 * the Free Software Foundation; either version 2 of the License, or
-	 * (at your option) any later version.
-	 *
-	 * This program is distributed in the hope that it will be useful,
-	 * but WITHOUT ANY WARRANTY; without even the implied warranty of
-	 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	 * GNU General Public License for more details.
-	 *
-	 * You should have received a copy of the GNU General Public License
-	 * along with this program; if not, write to the Free Software
-	 * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-	 *
- 	 */
+    /**
+     * @package Pushwoosh
+     * @version 2.2
+     */
+
+    /**
+    * Plugin Name: Pushwoosh
+    * Plugin URI: http://pushwoosh.com
+    * Description: Push notifications plugin for wordpress by Pushwoosh
+    * Author: Arello Mobile
+    * Author URI: http://www.arello-mobile.com/
+    * Version: 2.2
+    *
+    * Copyright 2013 Arello Mobile (email: support@arello-mobile.com)
+    * This program is free software; you can redistribute it and/or modify
+    * it under the terms of the GNU General Public License as published by
+    * the Free Software Foundation; either version 2 of the License, or
+    * (at your option) any later version.
+    *
+    * This program is distributed in the hope that it will be useful,
+    * but WITHOUT ANY WARRANTY; without even the implied warranty of
+    * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    * GNU General Public License for more details.
+    *
+    * You should have received a copy of the GNU General Public License
+    * along with this program; if not, write to the Free Software
+    * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+    *
+    */
 
 	error_reporting(E_ALL);
 	ini_set('display_errors', 0);
