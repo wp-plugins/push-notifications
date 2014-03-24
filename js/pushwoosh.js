015405a16b240a20593b621bbee7e7ee019b365a
pushwoosh <pushwoosh@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-24 16:02:32 +0000

    version 2.3.4
    git-svn-id: https://plugins.svn.wordpress.org/push-notifications/tags/2.3.4@880965 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/pushwoosh.js
+++ js/pushwoosh.js
@@ -2,14 +2,17 @@ function showLengthOver() {
 	max_length = 25;
 	critical_color = 'red';
 	default_color = '#b1b6c3';
-	title_length = document.getElementById('pushwoosh_safari_title').value.length;
-	result = max_length - title_length;
-	if (result < 6) {
-		document.getElementById('pushwoosh_length_over').style.color = critical_color;
-	} else {
-		document.getElementById('pushwoosh_length_over').style.color = default_color;
+	safari_title = document.getElementById('pushwoosh_safari_title');
+	if (safari_title != undefined) {
+		title_length = safari_title.value.length;
+		result = max_length - title_length;
+		if (result < 6) {
+			document.getElementById('pushwoosh_length_over').style.color = critical_color;
+		} else {
+			document.getElementById('pushwoosh_length_over').style.color = default_color;
+		}
+		document.getElementById('pushwoosh_length_over').innerHTML = result;
 	}
-	document.getElementById('pushwoosh_length_over').innerHTML = result;
 }
 
 document.addEventListener('DOMContentLoaded',
